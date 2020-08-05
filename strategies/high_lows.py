1b4d63093ed94ba8824b5c6fbf932e6e0295104a
Sebastian <>
2020-08-04 15:08:16 -1000

    renko

--- strategies/high_lows.py
+++ strategies/high_lows.py
@@ -1,5 +1,6 @@
 import backtrader as bt
 from svm_box_functions.utils import GenericCSV_SVM
+from indicators import Slope, BollingerBands
 
 """
 First try for this strategy.
@@ -26,16 +27,29 @@ class HL_Strategy(bt.Strategy):
         self.start_trade = None
         self.end_trade = None
 
+        self.previous_limit = None
+
         self.trail_price = None
         self.buyprice = None
         self.buycomm = None
 
-        n = 48
+        self.redline = None
+        self.blueline = None
 
-        self.highs = bt.indicators.Highest(self.data.lines.close(-n), period=n, subplot=False, plot=True)
-        self.lows = bt.indicators.Lowest(self.data.lines.close(-n), period=n, subplot=False, plot=True)
+        n = 12
 
-        self.ATR = bt.indicators.ATR()
+        # self.laghighs = bt.indicators.Highest(self.data.lines.close(-n), period=n, subplot=False, plot=True)
+        # self.highs = bt.indicators.Highest(self.data.lines.close, period=n, subplot=False, plot=True)
+        # self.lows = bt.indicators.Lowest(self.data.lines.close, period=n, subplot=False, plot=True)
+
+        # self.TR = bt.indicators.TrueRange()
+        # self.ATR = bt.indicators.AverageTrueRange()
+        # self.RSI = bt.indicators.RSI()
+        # self.ADX = bt.indicators.DirectionalIndicator()
+        # self.bband = BollingerBands(period=n, devfactor=2)
+        # self.slope = Slope()
+        # self.slow = bt.indicators.ExponentialMovingAverage(period=64*4)
+        # self.fast = bt.indicators.ExponentialMovingAverage(period=64)
 
         # self.avg = self.highs + self.lows
 
@@ -79,21 +93,63 @@ class HL_Strategy(bt.Strategy):
 
     def next(self):
         # Simply log the closing price of the series from the reference
-        self.log('Position: %.0f, Close: %.2f, H: %.2f, L: %.2f' % (self.position.size, self.dataclose[0],
-                                                                          self.highs[0], self.lows[0]))
+        self.log('Position: %.0f, Close: %.2f' % (self.position.size, self.dataclose[0]))
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
+            # self.order.price = self.bband.lines.top * 1.0
+          
             return
 
-        # Check if we are in the market
-        if not self.position:
 
-            if self.dataclose[0] > self.highs * 1.0:
-                self.start_trade = self.buy(size=1)
-                self.trail_price = self.start_trade.created.price
-                self.end_trade = self.sell(size=1, exectype=bt.Order.StopTrail, trailpercent=0.05)
-                self.log('BUY CREATE, %.2f, TRAIL PRICE %.2f' % (self.dataclose[0], self.trail_price))
+        # if self.dataclose < self.bband.lines.bot and not self.position:
+        #     self.redline = True
+        #
+        # if self.dataclose > self.bband.lines.top and self.position:
+        #     self.blueline = True
+
+        # if self.dataclose > self.bband.lines.mid and not self.position and self.redline:
+        #     # BUY, BUY, BUY!!! (with all possible default parameters)
+        #     self.log('BUY CREATE, %.2f' % self.dataclose[0])
+        #     # Keep track of the created order to avoid a 2nd order
+        #     self.order = self.buy()
+
+        # if not self.position:
+        #
+        #     if self.fast > self.slow and self.fast[-1] <= self.slow[-1]:
+        #         self.order = self.buy()
+        #         # self.end_trade = self.sell(size=1, exectype=bt.Order.StopTrail, trailpercent=0.05)
+        #         self.log('BUY CREATE, %.2f' % self.order.created.price)
+        #
+        #     if self.fast < self.slow and self.fast[-1] >= self.slow[-1]:
+        #         self.order = self.sell()
+        #         # self.end_trade = self.buy(size=1, exectype=bt.Order.StopTrail, trailpercent=0.05)
+        #         self.log('SELL CREATE, %.2f' % self.order.created.price)
+
+        # if self.dataclose > self.bband.lines.top * 1.01 and not self.position:
+        #     # BUY, BUY, BUY!!! (with all possible default parameters)
+        #     self.log('BUY CREATE, %.2f' % self.dataclose[0])
+        #     # Keep track of the created order to avoid a 2nd order
+        #     self.order = self.buy()
+
+        # if self.dataclose <= self.bband.lines.mid and self.position:
+        #     # SELL, SELL, SELL!!! (with all possible default parameters)
+        #     self.log('SELL CREATE, %.2f' % self.dataclose[0])
+        #     self.blueline = False
+        #     self.redline = False
+        #     # Keep track of the created order to avoid a 2nd order
+        #     self.order = self.sell()
+
+        # Check if we are in the market
+        # if not self.position:
+
+            # if self.dataclose[0] > self.laghighs * 1.01 \
+            #         and self.dataclose[0] > self.dataclose[-1] * 1.01\
+            #         and self.highs > self.laghighs:
+            #     self.start_trade = self.buy(size=1)
+            #     self.trail_price = self.start_trade.created.price
+            #     self.end_trade = self.sell(size=1, exectype=bt.Order.StopTrail, trailpercent=0.01)
+            #     self.log('BUY CREATE, %.2f, TRAIL PRICE %.2f' % (self.dataclose[0], self.trail_price))
 
             # if self.dataclose[0] < self.lows * 0.99:
             #     self.start_trade = self.sell(size=1)
@@ -101,15 +157,15 @@ class HL_Strategy(bt.Strategy):
             #     self.end_trade = self.buy(size=1, exectype=bt.Order.StopTrail, trailpercent=0.05)
             #     self.log('SELL CREATE, %.2f, TRAIL PRICE %.2f' % (self.dataclose[0], self.trail_price))
 
-        if self.position.size > 0:
-            if self.dataclose[0] <= self.highs:
-                self.cancel(self.end_trade)
-                self.order = self.sell(size=1)
-
-            # if self.position.size < 0:
-            #     if self.dataclose[0] >= self.lows:
-            #         self.cancel(self.end_trade)
-            #         self.order = self.buy(size=1)
+        # if self.position.size > 0:
+        #     if self.fast < self.slow:
+        #         # self.cancel(self.end_trade)
+        #         self.order = self.sell(size=1)
+        #
+        # if self.position.size < 0:
+        #     if self.fast > self.slow:
+        #         # self.cancel(self.end_trade)
+        #         self.order = self.buy(size=1)
 
 
 
@@ -126,13 +182,19 @@ if __name__ == '__main__':
 
     data = GenericCSV_SVM(dataname='data/candles_svm15min.csv')
 
+    # Convert to Renko
+    data.addfilter(bt.filters.Renko, size=1, align=10)
     cerebro.adddata(data)
+
+
 
     cerebro.broker.set_cash(1000000)
     cerebro.broker.setcommission(commission=0.1/100)
 
     # wr = bt.WriterFile(out='logs.csv')
     cerebro.addwriter(bt.WriterFile, csv=True, out='logs.csv')
+
+
 
     # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
